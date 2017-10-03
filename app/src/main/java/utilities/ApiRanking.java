7c1da56514dc1fb9469f34defabd67423df15ad4
maticn <maticnova@gmail.com>
2017-10-03 15:27:31 +0200

    ApiRanking created.

---
+++ app/src/main/java/utilities/ApiRanking.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import java.util.List;
+import models.Rank;
+
+public class ApiRanking extends AsyncTask<Void, Void, List<Rank>> {
+
+    // This is run in a background thread
+    @Override
+    protected List<Rank> doInBackground(Void... params) {
+        try {
+            List<Rank> response = ApiUtility.getRank("rankings");
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("error", e.toString());
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(List<Rank> coords) {
+        super.onPostExecute(coords);
+        //TODO: Do something with rankings. Show them in the gragment.
+    }
+}
--- app/src/main/java/utilities/ApiRecordCoordinate.java
+++ app/src/main/java/utilities/ApiRecordCoordinate.java
@@ -1,25 +1,15 @@
 package utilities;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.example.nermi.gitettip.UserActivity;
-import com.google.gson.Gson;
-
 import java.util.List;
-
 import models.RecordCoordinate;
-import models.Response;
-import models.User;
 
-public class ApiRecordCoordinate extends AsyncTask<Boolean, Void, List<RecordCoordinate>> {
+public class ApiRecordCoordinate extends AsyncTask<Void, Void, List<RecordCoordinate>> {
 
     // This is run in a background thread
     @Override
-    protected List<RecordCoordinate> doInBackground(Boolean... params) {
+    protected List<RecordCoordinate> doInBackground(Void... params) {
         try {
             List<RecordCoordinate> response = ApiUtility.getRecordCoordinate("recordscoordinates");
             return response;
@@ -33,7 +23,6 @@ public class ApiRecordCoordinate extends AsyncTask<Boolean, Void, List<RecordCoo
     @Override
     protected void onPostExecute(List<RecordCoordinate> coords) {
         super.onPostExecute(coords);
-        //TODO: Do something with this list of points - show them on the map.
-        int n = 5;
+        //TODO: Do something with this list of points (coords) - show them on the map.
     }
 }
