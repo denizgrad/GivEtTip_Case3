666275ef9b5a3e1bf4799f69b75e71dc00cb414b
maticn <maticnova@gmail.com>
2017-10-03 15:12:52 +0200

    Successful call to API to get the marker positions.

---
+++ app/src/main/java/models/RecordCoordinate.java
@@ -0,0 +1,52 @@
+package models;
+
+/**
+ * Created by Matic-ProBook on 3. 10. 2017.
+ */
+
+public class RecordCoordinate {
+    private int recordId;
+    private int authorId;
+    private String authorEmail;
+    private double gpsLatitude;
+    private double gpsLongitude;
+
+    public RecordCoordinate(Record r) {
+        this.recordId = r.getId();
+        this.authorId = r.getAuthor().getId();
+        this.authorEmail = r.getAuthor().getEmail();
+        this.gpsLatitude = r.getGpsLatitude();
+        this.gpsLongitude = r.getGpsLongitude();
+    }
+
+    public int getRecordId() {
+        return recordId;
+    }
+    public void setRecordId(int recordId) {
+        this.recordId = recordId;
+    }
+    public int getAuthorId() {
+        return authorId;
+    }
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+    public void setAuthorEmail(String authorEmail) {
+        this.authorEmail = authorEmail;
+    }
+    public double getGpsLatitude() {
+        return gpsLatitude;
+    }
+    public void setGpsLatitude(double gpsLatitude) {
+        this.gpsLatitude = gpsLatitude;
+    }
+    public double getGpsLongitude() {
+        return gpsLongitude;
+    }
+    public void setGpsLongitude(double gpsLongitude) {
+        this.gpsLongitude = gpsLongitude;
+    }
+}
