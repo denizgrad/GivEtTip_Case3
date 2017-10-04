7cbe5120ffdc2b6af6c96128b6ac1ce6336b4168
maticn <maticnova@gmail.com>
2017-10-04 15:40:06 +0200

    Enough of playing with leader board.

--- app/src/main/java/models/Rank.java
+++ app/src/main/java/models/Rank.java
@@ -1,10 +1,5 @@
 package models;
 
-import java.io.Console;
-import java.io.InterruptedIOException;
-import java.io.PrintStream;
-import java.math.BigDecimal;
-
 public class Rank {
 
     private int id;
@@ -48,46 +43,8 @@ public class Rank {
         String positionS = String.format("%4d", this.getPosition()).toString();
         String emailS = String.format("%s", this.getUserEmail()).toString();
         String scoreS = String.format("%04.1f", this.getScore()).toString();
-
-        String positionString = setOutputLength(this.getPosition(), 4);
-        int l = positionS.length();
-        String emailString = setOutputLength(this.getUserEmail(), 30);
-        int ll = emailString.length();
-        String scoreString = setOutputLength(this.getScore(), 8);
-        int lll = scoreString.length();
-        String space = "     ";
-
-        String res = positionString + space + emailString + space + scoreString;
-
-        String dol = Integer.toString(l) + ", " + Integer.toString(ll) + ", " + Integer.toString(lll) + ", " + Integer.toString(res.length());
-        System.out.println("LENGTHHHHHHHH: " + dol);
-
-        return res;
-
-//        return  positionS + "\t\t" + emailS + ":\t\t" + scoreS;
-//        return this.getPosition() + ": " + this.getUserEmail() + " - " + this.getScore();
+        return  positionS + "\t\t" + emailS + ":\t\t" + scoreS;
     }
-
-    public static String setOutputLength(int n, int length) {
-        String s = Integer.toString(n);
-        while (s.length() <= length)
-            s = " " + s;
-        return s;
-    }
-
-    public static String setOutputLength(double n, int length) {
-        String s = Double.toString(n);
-        while (s.length() <= length)
-            s = " " + s;
-        return s;
-    }
-
-    public static String setOutputLength(String n, int length) {
-        while (n.length() <= length)
-            n = n + " ";
-        return n;
-    }
-
     public Rank(int userId, int position, double score, String userEmail) {
         this.userId = userId;
         this.position = position;
